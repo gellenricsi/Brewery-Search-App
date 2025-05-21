188ff61811741d6b4905eeb88a684648a9f6eb53
gellenricsi <ricsi16555@gmail.com>
2025-05-21 13:19:46 +0200

    feat: create final app

---
+++ src/main/java/com/brewery/app/Controllers/BreweryController.java
@@ -0,0 +1,65 @@
+package com.brewery.app.Controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+
+@RestController
+public class BreweryController {
+
+    @GetMapping("/search")
+    public ResponseEntity<String> searchBrewery(
+            @RequestParam(required = false) String country,
+            @RequestParam(required = false) String type,
+            @RequestParam(required = false, defaultValue = "10") int resultsPerPage,
+            @RequestParam String id
+    ) {
+        StringBuilder url = new StringBuilder("https://api.openbrewerydb.org/v1/breweries?");
+        boolean hasParameter = false;
+
+        if (country != null && !country.isEmpty()) {
+            url.append("by_country=").append(country);
+            hasParameter = true;
+        }
+
+        if (type != null && !type.isEmpty()) {
+            if (hasParameter) url.append("&");
+            url.append("by_type=").append(type);
+            hasParameter = true;
+        }
+
+        if(hasParameter) url.append("&");
+
+        url.append("resultsPerPage=").append(resultsPerPage);
+
+        Response response = given().get(url.toString());
+
+        boolean found = response.jsonPath().getList("id", String.class).contains(id);
+
+        StringBuilder result = new StringBuilder();
+
+        if(found) {
+            result.append("Van találat a keresett ID-ra!\n\n");
+            result.append("Ország: ").append(country != null ? country : "Unknown").append("\n");
+            result.append("Típus: ").append(type != null ? type : "Unknown").append("\n");
+            result.append("Elem/oldal: ").append(resultsPerPage).append("\n");
+            result.append("Keresett ID: ").append(id).append("\n");
+        } else {
+            result.append("Nincs találat a keresett ID-ra. \n\n");
+            result.append("Ország: ").append(country != null ? country : "Unknown").append("\n");
+            result.append("Típus: ").append(type != null ? type : "Unknown").append("\n");
+            result.append("Elem/oldal: ").append(resultsPerPage).append("\n");
+            result.append("Keresett ID: ").append("FAKE ID").append("\n");
+        }
+
+        if(found) {
+            return ResponseEntity.status(201).body(result.toString());
+        } else {
+            return ResponseEntity.status(404).body(result.toString());
+        }
+    }
+}
